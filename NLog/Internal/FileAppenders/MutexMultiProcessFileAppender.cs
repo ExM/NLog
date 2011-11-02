7f1e4c9a57ebb6977e3044c38402ddb3ae1228cd
ExM <ExplorerMachine@gmail.com>
2011-11-03 01:20:49 +0400

    remove FileInfoHelper

--- NLog/Internal/FileAppenders/MutexMultiProcessFileAppender.cs
+++ NLog/Internal/FileAppenders/MutexMultiProcessFileAppender.cs
@@ -130,7 +130,35 @@ namespace NLog.Internal.FileAppenders
 		/// </returns>
 		public override bool GetFileInfo(out DateTime lastWriteTime, out long fileLength)
 		{
-			return FileInfoHelper.Helper.GetFileInfo(FileName, this.file.SafeFileHandle.DangerousGetHandle(), out lastWriteTime, out fileLength);
+			return GetFileInfo_Win(FileName, this.file.SafeFileHandle.DangerousGetHandle(), out lastWriteTime, out fileLength);
+		}
+
+		/// <summary>
+		/// Gets the information about a file.
+		/// </summary>
+		/// <param name="fileName">Name of the file.</param>
+		/// <param name="fileHandle">The file handle.</param>
+		/// <param name="lastWriteTime">The last write time of the file.</param>
+		/// <param name="fileLength">Length of the file.</param>
+		/// <returns>
+		/// A value of <c>true</c> if file information was retrieved successfully, <c>false</c> otherwise.
+		/// </returns>
+		private bool GetFileInfo_Win(string fileName, IntPtr fileHandle, out DateTime lastWriteTime, out long fileLength)
+		{
+			Win32FileNativeMethods.BY_HANDLE_FILE_INFORMATION fi;
+
+			if (Win32FileNativeMethods.GetFileInformationByHandle(fileHandle, out fi))
+			{
+				lastWriteTime = DateTime.FromFileTime(fi.ftLastWriteTime);
+				fileLength = fi.nFileSizeLow + (((long)fi.nFileSizeHigh) << 32);
+				return true;
+			}
+			else
+			{
+				lastWriteTime = DateTime.MinValue;
+				fileLength = -1;
+				return false;
+			}
 		}
 
 		private static string GetMutexName(string fileName)
