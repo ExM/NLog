8492ada702a78480551aed941d8709e52d7a2dc2
ExM <ExplorerMachine@gmail.com>
2011-11-06 15:04:04 +0400

    append override extension

---
+++ OverrideExtension/MyTarget.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+using NLog.Targets;
+	
+namespace MyOverrideExNamespace
+{
+	[Target("MyTarget")]
+	public class MyTarget : Target
+	{
+		protected override void Write(LogEventInfo logEvent)
+		{
+			// do nothing
+		}
+	}
+}
