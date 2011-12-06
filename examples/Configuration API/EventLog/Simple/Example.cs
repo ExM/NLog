82539a4798a87bcd8186c4d83963bfe7a6b17fcc
ExM <ExplorerMachine@gmail.com>
2011-12-06 20:59:10 +0400

    examples refactoring

---
+++ examples/Configuration API/EventLog/Simple/Example.cs
@@ -0,0 +1,19 @@
+using NLog;
+using NLog.Targets;
+
+class Example
+{
+	static void Main(string[] args)
+	{
+		EventLogTarget target = new EventLogTarget();
+		target.Source = "My Source";
+		target.Log = "Application";
+		target.MachineName = ".";
+		target.Layout = "${logger}: ${message}";
+
+		NLog.Config.SimpleConfigurator.ConfigureForTargetLogging(target, LogLevel.Debug);
+
+		Logger logger = LogManager.GetLogger("Example");
+		logger.Debug("log message");
+	}
+}
