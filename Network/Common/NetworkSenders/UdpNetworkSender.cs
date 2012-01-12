eb6491431c5a956edafbecffbc0f5764d1e07edb
ExM <ExplorerMachine@gmail.com>
2012-01-13 00:13:38 +0400

    queued udp messages (fix for DotNet)

--- Network/Common/NetworkSenders/UdpNetworkSender.cs
+++ Network/Common/NetworkSenders/UdpNetworkSender.cs
@@ -3,16 +3,23 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using NLog.Internal.NetworkSenders;
+using System.Collections.Generic;
 
 namespace NLog.Common.NetworkSenders
 {
 	/// <summary>
 	/// Sends messages over the network as UDP datagrams.
 	/// </summary>
-	internal class UdpNetworkSender : NetworkSender
+	internal partial class UdpNetworkSender : NetworkSender
 	{
+		private object _sync = new object();
+		private readonly Queue<Ticket> _pendingRequests = new Queue<Ticket>();
+		private Exception _pendingError = null;
+		private bool _asyncOperationInProgress = false;
+		private Action<Exception> _closeContinuation = null;
+		private Action<Exception> _flushContinuation = null;
+
 		private UdpClient _client;
-		private IPEndPoint _endpoint;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="UdpNetworkSender"/> class.
@@ -32,9 +39,9 @@ namespace NLog.Common.NetworkSenders
 		/// </summary>
 		protected override void DoInitialize()
 		{
-			_endpoint = ParseEndpointAddress(new Uri(Address), AddressFamily);
+			IPEndPoint ep = ParseEndpointAddress(new Uri(Address), AddressFamily);
 			_client = new UdpClient();
-			_client.Connect(_endpoint);
+			_client.Connect(ep);
 		}
 
 		/// <summary>
@@ -43,9 +50,20 @@ namespace NLog.Common.NetworkSenders
 		/// <param name="continuation">The continuation.</param>
 		protected override void DoClose(Action<Exception> continuation)
 		{
+			lock (_sync)
+			{
+				if (_asyncOperationInProgress)
+					_closeContinuation += continuation;
+				else
+					CloseSocket(continuation);
+			}
+		}
+
+		private void CloseSocket(Action<Exception> continuation)
+		{
 			Exception resultEx = null;
 
-			lock (this)
+			lock (_sync)
 			{
 				try
 				{
@@ -54,7 +72,7 @@ namespace NLog.Common.NetworkSenders
 				}
 				catch (Exception exception)
 				{
-					if(exception.MustBeRethrown())
+					if (exception.MustBeRethrown())
 						throw;
 					resultEx = exception;
 				}
@@ -63,6 +81,21 @@ namespace NLog.Common.NetworkSenders
 			}
 			if (continuation != null)
 				continuation(resultEx);
+		}
+
+		/// <summary>
+		/// Performs sender-specific flush.
+		/// </summary>
+		/// <param name="continuation">The continuation.</param>
+		protected override void DoFlush(Action<Exception> continuation)
+		{
+			lock (_sync)
+			{
+				if (!_asyncOperationInProgress && _pendingRequests.Count == 0)
+					continuation(null);
+				else
+					_flushContinuation += continuation;
+			}
 		}
 
 		/// <summary>
@@ -75,32 +108,77 @@ namespace NLog.Common.NetworkSenders
 		/// <remarks>To be overridden in inheriting classes.</remarks>
 		protected override void DoSend(byte[] bytes, int offset, int length, Action<Exception> asyncContinuation)
 		{
-			byte[] copy = new byte[length];
-			Array.Copy(bytes, offset, copy, 0, length);
+			var ticket = new Ticket() { Buffer = new byte[length], Continuation = asyncContinuation };
+			Array.Copy(bytes, offset, ticket.Buffer, 0, length);
 
-			lock (this)
+			lock(_sync)
+				_pendingRequests.Enqueue(ticket);
+
+			ProcessNextQueuedItem();
+		}
+
+		private void ProcessNextQueuedItem()
+		{
+			lock (_sync)
 			{
+				if(_asyncOperationInProgress)
+					return;
+
+				if (_pendingError != null)
+					while(_pendingRequests.Count != 0)
+						_pendingRequests.Dequeue().Continuation(_pendingError);
+
+				if (_pendingRequests.Count == 0)
+				{
+					var fc = _flushContinuation;
+					if (fc != null)
+					{
+						_flushContinuation = null;
+						fc(_pendingError);
+					}
+
+					var cc = _closeContinuation;
+					if (cc != null)
+					{
+						_closeContinuation = null;
+						CloseSocket(cc);
+					}
+
+					return;
+				}
+
+				var t = _pendingRequests.Dequeue();
+
+				_asyncOperationInProgress = true;
+
 				UdpClient clientCopy = _client;
 
-				Console.WriteLine("BeginSend");
-				clientCopy.BeginSend(copy, length, _endpoint, (ar) =>
-				{
-					Console.WriteLine("EndSend");
-					Exception resultEx = null;
-					try
-					{
-						clientCopy.EndSend(ar);
-					}
-					catch (Exception exception)
-					{
-						if (exception.MustBeRethrown())
-							throw;
-						resultEx = exception;
-					}
-					if(asyncContinuation != null)
-						asyncContinuation(resultEx);
-				}, null);
+				_client.BeginSend(t.Buffer, t.Buffer.Length,
+					(ar) => SendCompleted(clientCopy, t.Continuation, ar), null);
 			}
+		}
+
+		private void SendCompleted(UdpClient clientCopy, Action<Exception> continuation, IAsyncResult ar)
+		{
+			lock (_sync)
+			{
+				_asyncOperationInProgress = false;
+				try
+				{
+					clientCopy.EndSend(ar);
+				}
+				catch (Exception ex)
+				{
+					if (ex.MustBeRethrown())
+						throw;
+					_pendingError = ex;
+				}
+			}
+			
+			if(continuation != null)
+				continuation(_pendingError);
+
+			ProcessNextQueuedItem();
 		}
 	}
 }
